dded8069ea2ea704ca74a2781a3246d0c72579bc
Newroll <75225538+Newroll@users.noreply.github.com>
2023-04-26 14:55:50 +1200

    added level change to non-existent lvl 3

---
+++ src/Portal2.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+var entered = false
+
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+	$ContinueLable.visible = true
+
+
+func _on_body_exited(body):
+	entered = false
+	$ContinueLable.visible = false
+
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://src/level_3.tscn")
