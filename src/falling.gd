d182ffe1609e2042defd7448aedc92ab73f3124d
Newroll <75225538+Newroll@users.noreply.github.com>
2023-05-10 09:08:56 +1200

    attempt at fixing this insanity

--- src/falling.gd
+++ src/falling.gd
@@ -3,9 +3,8 @@ extends Area2D
 var velocity = Vector2()
 var urmom = 0
 
+func _on_body_entered(body):
+	urmom = 3
+
 func _physics_process(_delta):
 	velocity.y += urmom
-
-
-func _on_area_2d_body_entered(body):
-	urmom = 3
