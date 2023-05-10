6957af82ce35c87ba2df957f3359dd2985e68200
Newroll <75225538+Newroll@users.noreply.github.com>
2023-05-10 13:14:35 +1200

    finish lvl 2, no background

---
+++ src/Area2D.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+var pos = position.y
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(pos)
--- src/falling.gd
+++ src/falling.gd
@@ -1,10 +1,4 @@
 extends Area2D
 
-var velocity = Vector2()
-var urmom = 0
-
-func _on_body_entered(body):
-	urmom = 3
-
 func _physics_process(_delta):
-	velocity.y += urmom
+	pass
