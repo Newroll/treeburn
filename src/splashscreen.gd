17d0759023b82211ec78b98ef3799b886c1f990b
Jayd3nPye <127900744+Jayd3nPye@users.noreply.github.com>
2023-05-14 20:01:54 +1200

    Pause Menu & Ability to skip splash screen
    The Pause Menu has not been fully implemented and if there are error/merge conflicts then I will look at it another day. Other than that there are some things that I want to go over like changing the text for the coin counter along with these two similar errors caused by something to do with the portal and thats it. I'll work on stuff like transitions between scenes next time or something like that but cya on Wednesday.

--- src/splashscreen.gd
+++ src/splashscreen.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func _input(event):
+	if(event.is_action_pressed("skip_animation")):
+		go_title_screen()
 
 func go_title_screen():
 	get_tree().change_scene_to_file("res://src/intro.tscn")
 
-func _on_animation_player_animation_finished(anim_name):
+func _on_animation_player_animation_finished(_splashscreen_animation):
 	go_title_screen()
 	pass # Replace with function body.
+
