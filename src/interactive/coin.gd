ce70f24eb583efa033806a73b9c880e0dc762b9e
Newroll <75225538+Newroll@users.noreply.github.com>
2023-05-17 13:59:16 +1200

    lvl 1 improvements

--- src/interactive/coin.gd
+++ src/interactive/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-
-
 func _on_body_entered(body):
 	if body.name == "CharacterBody2D":
-		queue_free()
+		$AudioStreamPlayer.play()
+		self.visible = false
 		Main.coins += 1
+		await get_tree().create_timer(0.4).timeout
+		queue_free()
