316eaea7785ad3050078dd412dbb23377e9f5ae9
Newroll <75225538+Newroll@users.noreply.github.com>
2023-05-17 12:19:29 +1200

    more changes
    still more debugging to do

--- src/interactive/portal1.gd
+++ src/interactive/portal1.gd
@@ -15,4 +15,4 @@ func _on_body_exited(_body):
 func _process(_delta):
 	if entered == true:
 		if Input.is_action_just_pressed("enter"):
-			get_tree().change_scene_to_file("res://src/level_2.tscn")
+			get_tree().change_scene_to_file("res://src/levels/level_2.tscn")
