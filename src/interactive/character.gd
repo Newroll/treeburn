ce525173e6d1b719457ad2c1021ffba8a6c5fa1a
Newroll <75225538+Newroll@users.noreply.github.com>
2023-06-21 13:01:13 +1200

    initial changes

--- src/interactive/character.gd
+++ src/interactive/character.gd
@@ -3,7 +3,13 @@ extends CharacterBody2D
 signal _on_player_death()
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -300.0
+const wall_pushback = -50
+const wall_slide = 10
+
+var jump_counter = 0
+var double_jump = false
+var wall_sliding = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,19 +17,31 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() && wall_sliding == false:
 		velocity.y += gravity * delta
 		animated_sprite.animation = "default"
 	else:
+		jump_counter = 0
 		if(velocity.x == 0):
 			animated_sprite.animation = "default"
 		else:
 			animated_sprite.animation = "move"
+	
+	#wall sliding
+	if is_on_wall() && not is_on_floor():
+		animated_sprite.animation = "default"
+		velocity.x = wall_pushback
+		velocity.y = wall_slide
+		velocity.y += gravity * delta
+	else:
+		wall_sliding = false
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") && jump_counter < 2:
+		jump_counter += 1
 		velocity.y = JUMP_VELOCITY
 		$AudioStreamPlayer.play()
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -43,6 +61,7 @@ func _physics_process(delta):
 	var playerposition = get_position()
 	if(playerposition.y > 100):
 		position = Vector2(10,-20)
+		double_jump = true
 		Main.coins = 0
 		_on_player_death.emit()
 	
