9da2e169f70e98e99b74b3bc50602bc526a10863
Newroll <75225538+Newroll@users.noreply.github.com>
2023-07-27 12:26:48 +1200

    Revert "sprite changes & more level 3"
    This reverts commit 31fd90f845f3fcfda2f23d910126d2968c031cd0.

--- src/interactive/snow.gd
+++ src/interactive/snow.gd
@@ -4,32 +4,25 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var returnTo = get_position()
-var needToReset = false
 
 
 func _physics_process(delta):
-	
-	if position.y > -500:
-		if not is_on_floor():
-			velocity.y += gravity * delta
-		
-		var direction = -1
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = -1
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	get_position()
-	
 	if position.x < 100:
-		position = Vector2(1000, -1000)
-		needToReset = true
-		
-	if Main.levelTimeEclapsed % 6 == 0 && needToReset == true:
-		position = Vector2(returnTo.x, returnTo.y)
-		needToReset = false
+		position = Vector2(659, -449)
 	
-	if position.y > -500:
-		move_and_slide();
+	move_and_slide();
