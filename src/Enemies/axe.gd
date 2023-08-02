89ce8f7ec848b0c9d62c740df4dcb5c48d35e617
Jayd3nPye <127900744+Jayd3nPye@users.noreply.github.com>
2023-08-02 14:02:08 +1200

    player_detection

---
+++ src/Enemies/axe.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+var speed = 20.0
+var is_moving_left = false
+# aggro state
+var aggro = false
+@onready var raycast_left = $RayCast_Left
+@onready var raycast_right = $RayCast_Right
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	print(speed)
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	#Run functions
+	move_character()
+	move_and_slide()
+	detect_turn()
+
+func move_character():
+	if is_moving_left == true:
+		velocity.x = -speed
+	else:
+		velocity.x = speed
+
+func detect_turn():
+	if !raycast_left.is_colliding() && is_on_floor():
+		is_moving_left = false
+	if !raycast_right.is_colliding() && is_on_floor():
+		is_moving_left = true
+
+func _on_death_body_entered(body):
+		Main.dead.emit()
+
+func _on_player_detection_body_entered(body):
+	if body.is_in_group("player"):
+		aggro = true
+		speed = 60
+
+func _on_player_detection_body_exited(body):
+	if body.is_in_group("player"):
+		aggro = false
+		speed = 20
