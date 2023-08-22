b97b29e63ad32c6abf8825589f6b5eeff8400617
Robbin Xu <robbinxxy@gmail.com>
2023-08-22 14:49:10 +1200

    reinstate ben's work

--- src/interactive/coins.gd
+++ src/interactive/coins.gd
@@ -6,10 +6,12 @@ func _ready():
 func _on_body_entered(body):
 	if body.name == "CharacterBody2D":
 		$AudioStreamPlayer.play()
+		self.visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
 		Main.coins += 1
-		#CHANGE THIS!!!
 		if Main.worldHealth < 3600:
 			Main.worldHealth += 600
 		if Main.worldHealth > 3600:
 			Main.worldHealth = 3600
+		await get_tree().create_timer(0.4).timeout
 		queue_free()
