d53e5777c577d82ec12e5dd831bc1b0460d8c973
Jayd3nPye <127900744+Jayd3nPye@users.noreply.github.com>
2023-09-06 11:26:27 +1200

    Added one level

--- src/levels/level_2.gd
+++ src/levels/level_2.gd
@@ -16,7 +16,7 @@ func _process(_delta):
 		Main.coins = 0
 		Main.level = 3
 		get_tree().change_scene_to_file("res://src/levels/level_3.tscn")
-
+		
 	if Input.is_action_just_pressed("esc"):
 		get_tree().paused = true
 		$UI/PauseMenu.show()
