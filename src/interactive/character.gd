553006c9d389bc32890623ec2d03409b26f29e85
Jayd3nPye <127900744+Jayd3nPye@users.noreply.github.com>
2023-09-07 21:14:12 +1200

    level 6 start

--- src/interactive/character.gd
+++ src/interactive/character.gd
@@ -71,9 +71,10 @@ func _physics_process(delta):
 func check_state():
 	if Main.quicksand == true:
 		movement_data = load("res://src/interactive/QuicksandMovementData.tres")
-	elif Main.ice == true:
+	if Main.ice == true:
 		movement_data = load("res://src/interactive/IceMovementData.tres")
-	else:
+		print("Movement data ice")
+	if Main.ice == false && Main.quicksand == false:
 		movement_data = load("res://src/interactive/DefaultMovementData.tres")
 
 func apply_gravity(delta):
