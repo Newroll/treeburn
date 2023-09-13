54275f4969145d0b2400423f43fca86674c250be
Newroll <75225538+Newroll@users.noreply.github.com>
2023-09-13 15:11:49 +1200

    snow timers v1

--- src/interactive/snow_6_2.gd
+++ src/interactive/snow_6_2.gd
@@ -6,6 +6,11 @@ const JUMP_VELOCITY = -400
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var returnTo = get_position()
+@onready var snowTimer: Timer = $TimeoutTimer
+@onready var snowInterval: Timer = $IntervalTimer
+
+func _ready():
+	snowTimer.start()
 
 
 func _physics_process(delta):
@@ -25,8 +30,8 @@ func _physics_process(delta):
 	if position.x > 155:
 		position = Vector2(1000, -1000)
 		
-	if int(Main.timeEclapsed) % 6 == 0:
-		position = Vector2(returnTo.x, returnTo.y)
+	#if int(Main.timeEclapsed) % 6 == 0:
+		#position = Vector2(returnTo.x, returnTo.y)
 	
 	if position.y > -800:
 		move_and_slide();
@@ -35,3 +40,13 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body):
 	if body.name == "CharacterBody2D":
 		Main.snowHit += 1
+
+
+func _on_timeout_timer_timeout():
+	position = Vector2(returnTo.x, returnTo.y)
+	snowInterval.start()
+	
+
+
+func _on_interval_timer_timeout():
+	snowTimer.start()
