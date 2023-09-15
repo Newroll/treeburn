3e1f14fe1de79a2d9ae40bd8e8fce3b31f3417c1
Robbin Xu <robbinxxy@gmail.com>
2023-09-15 20:09:02 +1200

    zooming camera

--- src/levels/camera.gd
+++ src/levels/camera.gd
@@ -1,7 +1,12 @@
-extends Area2D
+extends Camera2D
 
-@onready
-var camera = get_node("Camera2D")
+var zoomLevels = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+var currentZoomLevel = 4
 
-func _on_body_entered(body):
-	camera.limit_top = -200;
+func _process(delta):
+	set_zoom(Vector2(zoomLevels[currentZoomLevel], zoomLevels[currentZoomLevel]))
+	
+	if Input.is_action_just_pressed("zoomIn") && currentZoomLevel > 0:
+		currentZoomLevel -= 1
+	if Input.is_action_just_pressed("zoomOut") && currentZoomLevel < 8:
+		currentZoomLevel += 1
