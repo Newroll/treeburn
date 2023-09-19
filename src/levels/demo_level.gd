46f026ea8432996957a8eadd87fc024d0dcd8d17
Jayd3nPye <127900744+Jayd3nPye@users.noreply.github.com>
2023-09-20 10:58:39 +1200

    fix fading between levels

--- src/levels/demo_level.gd
+++ src/levels/demo_level.gd
@@ -4,56 +4,71 @@ var music1 = preload("res://assets/music/musiclvl1.mp3")
 var music2 = preload("res://assets/music/musiclvl2.mp3")
 var music3 = preload("res://assets/music/musiclvl3.mp3")
 
-var setMusic1 = false
-var setMusic2 = false
-var setMusic3 = false
+var setLevel1= true
+var setLevel2 = false
+var setLevel3 = false
+
+func _ready():
+	$music.set_stream(music1)
+	$music.play()
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("esc"):
 		get_tree().paused = true
 		$UI/PauseMenu.show()
 		
-	if Main.playerPosition.x < 2013:
-		if setMusic1 == false:
-			if Main.playerPosition.x > 300:
-				transition()
-			$music.set_stream(music1)
-			setMusic1 = true
-			$music.play()
-		$bg1.show()
-		$bg2.hide()
-		$bg3.hide()
-		setMusic2 = false
-		setMusic3 = false
-	if Main.playerPosition.x > 2013 && Main.playerPosition.x < 3090:
-		if setMusic2 == false:
-			transition()
-			$music.set_stream(music2)
-			setMusic2 = true
-			$music.play()
-		$bg1.hide()
-		$bg2.show()
-		$bg3.hide()
-		setMusic1 = false
-		setMusic3 = false
-	if Main.playerPosition.x > 3090:
-		$bg1.hide()
-		$bg2.hide()
-		$bg3.show()
-		setMusic1 = false
-		setMusic2 = false
-		if setMusic3 == false:
-			transition()
-			$music.set_stream(music3)
-			setMusic3 = true
-			$music.play()
+	if Main.playerPosition.x < 1800 && setLevel1 == false:
+		setLevel1 = true
+		setLevel2 = false
+		setLevel3 = false
+		transition()
+
+	if Main.playerPosition.x > 1800 && Main.playerPosition.x < 2920 && setLevel2 == false:
+		setLevel1 = false
+		setLevel2 = true
+		setLevel3 = false
+		transition()
+
+	if Main.playerPosition.x > 2920 && setLevel3 == false:
+		setLevel1 = false
+		setLevel2 = false
+		setLevel3 = true
+		transition()
+
 	if Main.playerPosition.y > 200:
 		Main.unanimated_death()
 
 func transition():
-	$CanvasLayer.show()
+	#$CanvasLayer.show()
 	get_tree().paused = true
 	$CanvasLayer/AnimationPlayer.play("fade")
-	await get_tree().create_timer(4).timeout
+
+func transition_done():
 	get_tree().paused = false
-	$CanvasLayer.hide()
+	#$CanvasLayer.hide()
+
+	if Main.playerPosition.x > 2920:
+		$bg1.hide()
+		$bg2.hide()
+		$bg3.show()
+
+		$music.set_stream(music3)
+		$music.play()
+
+	if Main.playerPosition.x > 1800 && Main.playerPosition.x < 2920:
+		$bg1.hide()
+		$bg2.show()
+		$bg3.hide()
+
+		$music.set_stream(music2)
+		$music.play()
+	
+	if Main.playerPosition.x < 1800:
+		$bg1.show()
+		$bg2.hide()
+		$bg3.hide()
+		
+		$music.set_stream(music1)
+		$music.play()
+
