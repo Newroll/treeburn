728e4586a851ed8f50683f779a584ac23e829518
Jayd3nPye <127900744+Jayd3nPye@users.noreply.github.com>
2024-04-19 12:05:58 +1200

    splashscreen fix

--- src/ui/intro.gd
+++ src/ui/intro.gd
@@ -9,20 +9,17 @@ func _ready():
 	Main.gameComplete = true
 	Main.health = 3
 
-func _on_new_game_button_pressed():
-	$title.hide()
-	$NewGame_Button.hide()
-	$creditsbutton.hide()
-	$Label.show()
-	$Letter.show()
-	$AnimationPlayer.play("splashscreen_animation")
 
-func _on_animation_player_animation_finished(anim_name):
-	$ColorRect.show()
-	$AnimationPlayer.play("fadetoblack")
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_file("res://src/levels/level_0.tscn")
+func _on_new_game_button_pressed():
+	#$title.hide()
+	#$NewGame_Button.hide()
+	#$creditsbutton.hide()
+	#$Label.show()
+	#$Letter.show()
+	#$AnimationPlayer.play("splashscreen_animation")
 	
+	#I made it that the splashscreen happens on launch instead of on new game
+	get_tree().change_scene_to_file("res://src/levels/level_0.tscn")
 
 
 func _on_creditsbutton_pressed():
