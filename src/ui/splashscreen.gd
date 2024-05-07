e8fdcfe37eb41073439930006e37a5af6465e0a5
Jayd3nPye <127900744+Jayd3nPye@users.noreply.github.com>
2024-05-08 11:09:06 +1200

    splashscreen fix

--- src/ui/splashscreen.gd
+++ src/ui/splashscreen.gd
@@ -5,6 +5,6 @@ func _input(event):
 	if(event.is_action_pressed("enter")):
 		get_tree().change_scene_to_file("res://src/ui/intro.tscn")
 
-func _on_animation_player_animation_finished(_splashscreen_animation):
+func _on_animation_player_animation_finished(splashscreenAnimation):
 	get_tree().change_scene_to_file("res://src/ui/intro.tscn")
 
